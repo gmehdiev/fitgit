f3c34c4f5982fb9967543be133cfc1b50e2f336d
DNS <gmehdiev@gmail.com>
2021-04-26 20:15:55 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick();
+    $('.slider__inner').slick({
+        speed: 500,
+        adaptiveHeight: false,
+        prevArrow:'<button type="button" class="slick-prev"><img src="../icon/chevron-left-solid.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="../icon/chevron-right-solid.png"></button>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive:[
+            {
+                breakpoint: 1050,
+                settings: {
+                  dots: false,
+                  arrows: false
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+              }
+        ]
+      });
   });
